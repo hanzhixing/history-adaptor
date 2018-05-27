45ae98a37e3eba3b8eda37dd714b99bbcc0bad19
Zhixin Han <zhixing.han.0409@gmail.com>
2018-05-27 19:01:33 +0800

    update

--- demo/index.js
+++ demo/index.js
@@ -4,22 +4,12 @@ import createHashHistory from 'history/createHashHistory';
 import {link} from '../lib';
 import rules from './rules';
 
+window.addEventListener('popstate', event => {
+    console.log(event);
+});
+
 let historyA = createMemoryHistory();
 let historyB = createMemoryHistory();
-
-console.log(historyA);
-console.log(historyB);
-
-// const [
-//     linkedHistoryA,
-//     linkedHistoryB,
-// ] = link(
-//     [
-//         {key: 'A', history: historyA},
-//         {key: 'B', history: historyB},
-//     ]
-//     , rules,
-// );
 
 const linked = link(
     rules,
@@ -34,11 +24,17 @@ console.log(`A (${historyA.index}/${historyA.length})`);
 console.log(`B (${historyB.index}/${historyB.length})`);
 console.log('---');
 
-console.log('[A].listen: "[A] -> locatin, action"');
-historyA.listen((location, action) => console.log('[A] -> ', location, action));
+console.log('[A].listen: "[A] 1-> locatin.pathname, action"');
+historyA.listen((location, action) => console.log('[A] 1-> ', location.pathname, action, historyA.length));
 
-console.log('[B].listen: "[B] -> location, action"');
-historyB.listen((location, action) => console.log('[B] -> ', location, action));
+console.log('[A].listen: "[A] 2-> locatin.pathname, action"');
+historyA.listen((location, action) => console.log('[A] 2-> ', location.pathname, action, historyA.length));
+
+console.log('[B].listen: "[B] 1-> location.pathname, action"');
+historyB.listen((location, action) => console.log('[B] 1-> ', location.pathname, action, historyB.length));
+
+console.log('[B].listen: "[B] 2-> location.pathname, action"');
+historyB.listen((location, action) => console.log('[B] 2-> ', location.pathname, action, historyB.length));
 
 console.log('---');
 
@@ -86,13 +82,6 @@ console.log('---');
 
 console.log('try goForward()');
 historyA.goForward();
-
-console.log(`A (${historyA.index}/${historyA.length})`);
-console.log(`B (${historyB.index}/${historyB.length})`);
-console.log('---');
-
-console.log('try go(-1)');
-historyA.go(-1);
 
 console.log(`A (${historyA.index}/${historyA.length})`);
 console.log(`B (${historyB.index}/${historyB.length})`);
