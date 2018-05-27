77903d3ec5e5957c4122e6408478e2289cdca4c3
Zhixin Han <zhixing.han.0409@gmail.com>
2018-05-27 14:51:24 +0800

    except length, index

--- demo/index.js
+++ demo/index.js
@@ -7,6 +7,9 @@ import rules from './rules';
 let historyA = createMemoryHistory();
 let historyB = createMemoryHistory();
 
+console.log(historyA);
+console.log(historyB);
+
 // const [
 //     linkedHistoryA,
 //     linkedHistoryB,
@@ -26,21 +29,71 @@ const linked = link(
 
 historyA = linked['A'];
 historyB = linked['B'];
-const listenCallbackStackA = linked.listenCallbackStackA;
 
-console.log('[A].listen: "[A] -> path"');
-historyA.listen((location, action) => console.log('[A] -> ', location.pathname));
+console.log('---');
 
-console.log('[B].listen: "[B] -> path"');
-historyB.listen((location, action) => console.log('[B] -> ', location.pathname));
+console.log(historyA);
+console.log(historyB);
+
+console.log('---');
+
+console.log('[A].listen: "[A] -> locatin, action"');
+historyA.listen((location, action) => console.log('[A] -> ', location, action));
+
+console.log('[B].listen: "[B] -> location, action"');
+historyB.listen((location, action) => console.log('[B] -> ', location, action));
+
+console.log('---');
 
 console.log('[A].push("/a/1")');
 historyA.push('/a/1');
 
-// console.log('push [/456] to [A]');
-// historyA.push('/456');
+console.log('---');
 
-// console.log('push [/yyy] to [B]');
-// historyB.push('/yyy');
+console.log('[A].push("/a/2")');
+historyA.push('/a/2');
 
-console.log(listenCallbackStackA);
+console.log('---');
+
+console.log('[B].push("/b/1")');
+historyB.push('/b/1');
+
+console.log('---');
+
+console.log('[B].push("/b/2")');
+historyB.push('/b/2');
+
+console.log('---');
+
+console.log('[A].push("/a/3")');
+historyA.push('/a/3');
+
+console.log('---');
+
+console.log('[A].push("/a/4")');
+historyA.push('/a/4');
+
+console.log('---');
+
+console.log('[A].push("/a/5")');
+historyA.push('/a/5');
+
+console.log('---');
+
+console.log('try go(-1)');
+historyA.go(-1);
+
+console.log('---');
+
+console.log('try go(-1)');
+historyA.go(-1);
+
+console.log('---');
+
+console.log('try go(-1)');
+historyA.go(-1);
+
+console.log('---');
+
+console.log('try go(-1)');
+historyA.go(-1);
